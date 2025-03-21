2d58c94dc36a8f81daaf1fe8fcfd01730ca8ac00
EndlessC0ding <endlesscoding@yahoo.com>
2025-03-21 23:29:11 +0300

    cr pst-cntrllr

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -21,13 +21,83 @@ const PostController = {
     }
   },
   getAllPosts: async (req, res) => {
-    res.send("getAllPosts");
+    const userId = req.user.userId;
+    try {
+      const posts = await prisma.post.findMany({
+        include: {
+          likes: true,
+          author: true,
+          comments: true
+        },
+        orderBy: {
+          createdAt: "desc"
+        }
+
+      });
+      const postWitLikeInfo = posts.map(post => ({ ...post, likedByUser: post.likes.some(like => like.userId === userId) }));
+      res.json(postWitLikeInfo);
+
+    } catch (error) {
+      console.error({ error: "Get all posts error", error });
+      res.status(500).json({ error: "Internal server error" });
+    }
   },
   getPostById: async (req, res) => {
-    res.send("getPostById");
+    const { id } = req.params;
+    const userId = req.user.userId;
+    try {
+
+      const post = await prisma.post.findUnique({
+        where: { id },
+        include: {
+          comments: {
+            include: {
+              user: true
+            }
+          },
+          likes: true,
+          author: true
+        },
+      });
+
+      if (!post) {
+        return res.status(404).json({ error: "Пост не найденг" });
+      }
+      const postWitLikeInfo = {
+        ...post, likedByUser: post.likes.some(like => like.userId === userId)
+      };
+
+      res.json(postWitLikeInfo);
+    } catch (error) {
+      console.error({ error: "Get post by id error", error });
+      res.status(500).json({ error: "Internal server error" });
+    }
+
   },
+
   deletePost: async (req, res) => {
-    res.send("deletPost");
+    const { id } = req.params;
+    const post = await prisma.post.findUnique({
+      where: { id }
+    });
+    if (!post) {
+      return res.status(404).json({ error: "Пост не найденг" });
+    }
+    if (post.authorId !== req.user.userId) {
+      return res.status(403).json({ error: "Нет доступа" });
+    }
+
+    try {
+      const transaction = await prisma.$transaction([
+        prisma.comment.deleteMany({ where: { postId: id } }),
+        prisma.like.deleteMany({ where: { postId: id } }),
+        prisma.post.delete({ where: { id } }),
+      ]);
+      res.json(transaction);
+    } catch (error) {
+      console.error({ error: "Delete post error", error });
+      res.status(500).json({ error: "Internal server error" });
+    }
   },
 
 };
